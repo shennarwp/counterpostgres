45fc7119b6ed82a1f6c1894a791f1d584b0facc8
shennarwp <shennawew@outlook.com>
2019-09-23 09:17:28 +0200

    initial programm

--- postgres/slave/docker-entrypoint.sh
+++ postgres/slave/docker-entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-if [ ! -s "${PGDATA}/PG_VERSION" ]; then
-echo "*:*:*:${PG_REP_USER}:${PG_REP_PASSWORD}" > ~/.pgpass
 
-chmod 0600 ~/.pgpass
+# PING MASTER, IF EXIST START AS SLAVE
+if ping -c 1 -W 1 ${PG_MASTER_HOST:?missing environment variable. PG_MASTER_HOST must be set}; then
 
-until ping -c 1 -W 1 ${PG_MASTER_HOST:?missing environment variable. PG_MASTER_HOST must be set}
-    do
-        echo "Waiting for master to ping..."
-        sleep 1s
-done
-until pg_basebackup -h ${PG_MASTER_HOST} -D ${PGDATA} -U ${PG_REP_USER} -vP -W
-    do
-        echo "Waiting for master to connect..."
-        sleep 1s
-done
+	# IF NEVER INITIALIZED BEFORE
+	if [ ! -s "${PGDATA}/PG_VERSION" ]; then
+		echo "*:*:*:${PG_REP_USER}:${PG_REP_PASSWORD}" > ~/.pgpass
+		chmod 0600 ~/.pgpass
 
-echo "host replication all 0.0.0.0/0 md5" >> "${PGDATA}/pg_hba.conf"
+		# RESTORE FROM MASTER BACKUP
+		until pg_basebackup -h ${PG_MASTER_HOST} -D ${PGDATA} -U ${PG_REP_USER} -vP -W
+	    do
+	        echo "Waiting for master to connect..."
+	        sleep 1s
+		done
+		echo "host replication all 0.0.0.0/0 md5" >> "${PGDATA}/pg_hba.conf"
 
+# RECOVERY CONFIGURATION FILE
 set -e
 
 cat > ${PGDATA}/recovery.conf <<EOF
@@ -27,10 +27,15 @@ trigger_file = '/tmp/touch_me_to_promote_to_me_master'
 EOF
 chown ${POSTGRES_USER} ${PGDATA} -R
 chmod 700 ${PGDATA} -R
+
+	fi
+
 fi
 
+# REPLICATION LEVEL
 sed -i 's/wal_level = hot_standby/wal_level = replica/g' ${PGDATA}/postgresql.conf
 
+# DIRECTORY FOR ARCHIVE
 mkdir -p ${PGDATA}/archive
 chmod 700 ${PGDATA}/archive
 chown -R ${POSTGRES_USER}:${POSTGRES_USER} ${PGDATA}/archive
